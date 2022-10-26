6172c7d442595ccdd80c755937a8cde98764e225
Celso Teofilo da Silva <90398200+celsoteofilo@users.noreply.github.com>
2022-10-26 19:56:01 -0300

    ok up

---
+++ Aula 2 = 26-10-22.py
@@ -0,0 +1,6 @@
+nome = input("Qual e seu nome: ")
+idade = int(input('Qual e sua idade: '))
+
+dobro = idade * 2
+print('Seu  nome e {} e sua idade e {}'.format(nome,idade))
+print(f'O dobro da sua idade e {dobro}')
