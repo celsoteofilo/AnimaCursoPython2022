f978163196c86450acc2fd48c527199a0367e161
Celso Teofilo da Silva <90398200+celsoteofilo@users.noreply.github.com>
2022-10-26 20:26:44 -0300

    UP

--- Aula 2 = 26-10-22.py
+++ Aula 2 = 26-10-22.py
@@ -4,3 +4,6 @@ idade = int(input('Qual e sua idade: '))
 dobro = idade * 2
 print('Seu  nome e {} e sua idade e {}'.format(nome,idade))
 print(f'O dobro da sua idade e {dobro}')
+
+if idade >= 18:
+    print("Voce e maior de idade ")
